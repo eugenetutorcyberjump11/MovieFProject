259585f097f3029a50fcf6859957c3c91953afdc
demenko <eugenetutor@gmail.com>
2024-05-10 19:29:28 +0300

    Add project files.

---
+++ MovieFProject/Movie.cs
@@ -0,0 +1,16 @@
+﻿public class Movie
+{
+    public string Title { get; private set; }
+    public string Director { get; private set; }
+    public int Year { get; private set; }
+    public bool IsAvaiblable { get; set; }
+
+    public Movie(string title, string director, int year)
+    {
+        Title = title;
+        Director = director;
+        Year = year;
+        IsAvaiblable = true;
+    }
+
+}
---
+++ MovieFProject/User.cs
@@ -0,0 +1,33 @@
+﻿public class User
+{
+    public string Name { get; private set; }
+    public List<Movie> WatchedMovies { get; private set; }
+    
+    public User (string name)
+    {
+        Name = name;
+        WatchedMovies = new List<Movie> ();
+    }
+
+
+    public void WatchMovie(Movie movie)
+    {
+        if (movie.IsAvaiblable)
+        {
+            movie.IsAvaiblable = false;
+            WatchedMovies.Add(movie);
+            Console.WriteLine($"{Name} is watching {movie.Title}");
+        }
+        else
+        {
+            Console.WriteLine("This movie is currently unavailable");
+        }
+    }
+
+    public void ReturnMovie(Movie movie)
+    {
+        movie.IsAvaiblable = true;
+        WatchedMovies.Remove(movie);
+        Console.WriteLine($"{Name} has returned {movie.Title}");
+    }
+}
